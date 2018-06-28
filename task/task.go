720b44e29f1f7b0cbe906898f8bac68e84579e4d
John Shiver <johneshiver@gmail.com>
2018-06-28 09:54:08 -0400

    Added task state

--- task/task.go
+++ task/task.go
@@ -3,11 +3,9 @@ package task
 import "sync"
 
 type TaskRunner interface {
-	Run(wg *sync.WaitGroup)
-
+	Run()
 	// requires TaskRuner to have an embedded Task
 	GetTask() *Task
-	GetState() string
 }
 
 type Task struct {
@@ -24,6 +22,44 @@ func NewTask(name string, children []TaskRunner, parent *Task) *Task {
 		children:        children,
 		parent:          parent,
 		results_channel: make(chan string),
-		state:           "not_running",
+		state:           "waiting",
 	}
+}
+
+func (ts *Task) GetState() string {
+	return ts.state
+
+}
+
+func (ts *Task) SetState(new_state string) (string, error) {
+	valid_states := []string{
+		"waiting",
+		"running",
+		"complete",
+	}
+
+	valid_state_param := false
+	for _, state := range valid_states {
+		if new_state == state {
+			valid_state_param = true
+			break
+		}
+
+	}
+
+	if !valid_state_param {
+		return "", error
+	}
+
+	ts.state = new_state
+	return ts.state, nil
+
+}
+
+func RunTask(tsk_runner TaskRunner, wg *sync.WaitGroup) {
+	tsk_runner.GetTask().SetState("running")
+	tsk_runner.Run()
+	wg.Done()
+	tsk_runner.GetTask().SetState("complete")
+
 }
