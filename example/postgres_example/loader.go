d180d31934079f80c518c9b3bded65bbc57fe6a0
johnshiver <johneshiver@gmail.com>
2018-07-18 00:12:16 -0400

    first pass at getting postgres working

--- example/postgres_example/loader.go
+++ example/postgres_example/loader.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 
 	"github.com/johnshiver/plankton/task"
 )
@@ -13,10 +16,25 @@ type PostgresLoader struct {
 	task       *task.Task
 }
 
+type Rating struct {
+	gorm.Model
+	user_id  int
+	movie_id int
+	rating   float64
+}
+
 func (pl *PostgresLoader) Run() {
 
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=mytestdb password=mysecretpassword sslmode=disable")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Rating{})
+
 	for result := range pl.GetTask().ResultsChannel {
-		//		fmt.Println(result)
 		results := []byte(result)
 		var new_line []string
 
@@ -24,7 +42,17 @@ func (pl *PostgresLoader) Run() {
 		if err != nil {
 			log.Panic(err)
 		}
-		fmt.Println(new_line[0])
+
+		user_id, _ := strconv.Atoi(new_line[0])
+		movie_id, _ := strconv.Atoi(new_line[1])
+		rating, _ := strconv.ParseFloat(new_line[2], 64)
+
+		db.Create(&Rating{
+			user_id:  user_id,
+			movie_id: movie_id,
+			rating:   rating,
+		})
+
 	}
 
 }
--- example/postgres_example/main.go
+++ example/postgres_example/main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	csv_runner := NewCSVRunner("/Users/js231813/go/src/github.com/johnshiver/plankton/example/postgres_example/the-movies-dataset/movies_metadata.csv")
+	// csv_runner := NewCSVRunner("/Users/js231813/go/src/github.com/johnshiver/plankton/example/postgres_example/the-movies-dataset/movies_metadata.csv")
+	// csv_runner2 := NewCSVRunner("/Users/js231813/go/src/github.com/johnshiver/plankton/example/postgres_example/the-movies-dataset/links.csv")
+	// csv_runner3 := NewCSVRunner("/Users/js231813/go/src/github.com/johnshiver/plankton/example/postgres_example/the-movies-dataset/credits.csv")
+	csv_runner4 := NewCSVRunner("/Users/js231813/go/src/github.com/johnshiver/plankton/example/postgres_example/the-movies-dataset/ratings.csv")
 
 	pg_loader := NewPostgresLoader("some_table")
-	pg_loader.GetTask().AddChildren(csv_runner)
+	pg_loader.GetTask().AddChildren(csv_runner4)
 
 	my_scheduler, err := scheduler.NewTaskScheduler(pg_loader)
 	if err != nil {
