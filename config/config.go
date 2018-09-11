f70d120b42eda40c67413d1d0afc69ebd70276a1
John Shiver <johneshiver@gmail.com>
2018-09-10 21:19:56 -0400

    added config + beginnings of scheduler test

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/spf13/viper"
+)
+
+var c Config
+
+func init() {
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/plankton")
+	viper.AddConfigPath(".")
+
+	/*
+			initalize config
+		        from yaml, get the database config (for now sqlite or postgres)
+			then determine if the connection string is valid
+	*/
+
+}
+
+type Config struct {
+	DataBase *gorm.DB
+}
+
+func GetConfig() Config {
+	return c
+}
+
+func ReadConfig() {
+	err := viper.ReadInConfig()
+	log.Printf("Using configuration file: %s\n", viper.ConfigFileUsed())
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
---
+++ scheduler/scheduler_test.go
@@ -0,0 +1,11 @@
+package schduler
+
+import "testing"
+
+func TestSaveSchedulerDag(t *testing.T) {
+
+}
+
+func TestRecreateStoredDag(t *testing.T) {
+
+}
